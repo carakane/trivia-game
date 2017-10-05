908a9a93766086b843765f7dd0c90c3be792ea96
c <github email address>
2017-10-05 11:23:12 -0400

    fix import, missing BrowserRouter

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Game from './Game'
@@ -16,7 +17,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
+        <Router>
+          <NavBar />
+        </Router>
         <Game />
       </div>
     );
