495093d9eecb4e8d93bc7d3580a81d98414af207
c <github email address>
2017-10-05 10:49:06 -0400

    add Game and setup basic file

---
+++ client/src/containers/Game.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+class Game extends Component {
+    constructor(){
+      super()
+  
+      this.setState = {
+  
+      }
+    }
+    
+    render() {
+      return (
+        <div className="Game">
+          
+        </div>
+      );
+    }
+  }
+  
+  export default Game;
+  
