cb0ea443f881750874d867331c6a69b4f697ffd6
c <github email address>
2017-10-06 10:35:18 -0400

    finesse display of clue

--- client/src/components/Clue.js
+++ client/src/components/Clue.js
@@ -2,16 +2,14 @@ import React from 'react';
 var titleize = require('titleize');
 
 const Clue = ({questions}) => {
-  debugger;
-
   return(
     <div>
       {questions.map(question =>
         <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-        <div className="value">${question.value}</div>
-        <div className="category">{titleize(question.category.title)}</div>
-        <div className="clue">{question.question}</div>
-        <div className="answer">{titleize(question.answer)}</div>
+        <div className="value">${question.value || 3000}</div>
+        <div className="category">Category: {titleize(question.category.title)}</div>
+        <div className="clue">Question: {question.question}</div>
+        <div className="answer">Answer: {titleize(question.answer)}</div>
       </div>)}
     </div>
   )
