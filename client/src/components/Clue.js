f7639df05a8dd3330af85a479aaaf56e859fa1c0
c <github email address>
2017-10-09 12:01:59 -0400

    begin to style clues as Panels

--- client/src/components/Clue.js
+++ client/src/components/Clue.js
@@ -1,4 +1,4 @@
-import { Button } from 'react-bootstrap';
+import { Button, Panel } from 'react-bootstrap';
 import React, {Component} from 'react';
 var titleize = require('titleize');
 
@@ -50,18 +50,20 @@ class Clue extends Component {
   
   render(){
     return(
-      <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-        <div className="value">${this.props.question.value || 3000}</div>
+      <Panel header={'$' + this.props.question.value || 3000} bsStyle="info">
+      {/* <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}> */}
+        {/* <div className="value">${this.props.question.value || 3000}</div> */}
         <div className="category">Category: {titleize(this.props.question.category.title)}</div>
         <div className="clue">Question: {this.props.question.question}</div>
         <div className="answerDIV">{this.state.question.isAnswered === false ? <Button onClick={() => this.onAnswer(this.props.question.id)}>Ready to Answer?</Button>
           :<div> 
           <div className="answer">Answer: {titleize(this.props.question.answer)}</div>
-          <Button bsStyle="primary" disabled={this.state.question.disabled} id={this.props.question.id + '-correct'} onClick={() => this.onCorrect(this.props.question.id)}>I was right!</Button>
-          <Button bsStyle="danger" disabled={this.state.question.disabled} id={this.props.question.id + '-incorrect'} onClick={() => this.onIncorrect(this.props.question.id)}>I was wrong!</Button>
+          <Button bsStyle="info" disabled={this.state.question.disabled} id={this.props.question.id + '-correct'} onClick={() => this.onCorrect(this.props.question.id)}>I was right!</Button>
+          <Button bsStyle="success" disabled={this.state.question.disabled} id={this.props.question.id + '-incorrect'} onClick={() => this.onIncorrect(this.props.question.id)}>I was wrong!</Button>
           </div>}
-        </div>
+        {/* </div> */}
       </div>
+      </Panel>
     )
   }  
 }
