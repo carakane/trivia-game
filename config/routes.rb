b8ab022305cfce0925c0fe5cb4a1d14a14b4d434
c <github email address>
2017-10-09 08:56:28 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :games
+
+   scope '/api' do
+    get :games, to: 'games#index'
+    post :games, to: 'games#create'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
